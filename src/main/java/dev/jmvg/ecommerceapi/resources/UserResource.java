d0443586833fc8e0cf3215516cab94c4a84baf4f
Lenk <jmvg.comp@gmail.com>
2020-01-30 00:13:20 -0300

    User insert

--- src/main/java/dev/jmvg/ecommerceapi/resources/UserResource.java
+++ src/main/java/dev/jmvg/ecommerceapi/resources/UserResource.java
@@ -4,11 +4,10 @@ import dev.jmvg.ecommerceapi.entities.User;
 import dev.jmvg.ecommerceapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,6 +31,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
