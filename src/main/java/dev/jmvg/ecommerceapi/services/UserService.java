7c4e0ee31670e0e1a483fda7b5825b0aab070cba
Lenk <jmvg.comp@gmail.com>
2020-02-02 00:16:56 -0300

    Exception handler - findById

--- src/main/java/dev/jmvg/ecommerceapi/services/UserService.java
+++ src/main/java/dev/jmvg/ecommerceapi/services/UserService.java
@@ -2,6 +2,7 @@ package dev.jmvg.ecommerceapi.services;
 
 import dev.jmvg.ecommerceapi.entities.User;
 import dev.jmvg.ecommerceapi.repositories.UserRepository;
+import dev.jmvg.ecommerceapi.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +24,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
